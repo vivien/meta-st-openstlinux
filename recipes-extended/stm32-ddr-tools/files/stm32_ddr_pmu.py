10bc68401eb0a434b656c5a2cc1d81cba8b22d2e
Christophe Priouzeau <christophe.priouzeau@foss.st.com>
2024-10-31 13:23:30 +0100

    STM32 DDR TOOLS: add DDR specific events on MP2
    Update events list with LPDDR specific events in case of LPDDR.
    Simplify the found_perf function.
    
    Change-Id: I6516184f0396f124e11528f6b1e4ee6c0d1faf24
    Signed-off-by: Christophe Priouzeau <christophe.priouzeau@foss.st.com>

--- recipes-extended/stm32-ddr-tools/files/stm32_ddr_pmu.py
+++ recipes-extended/stm32-ddr-tools/files/stm32_ddr_pmu.py
@@ -5,19 +5,13 @@ import re
 import sys
 import os
 
-def found_perf_with_address(address):
-        prefix = "/proc/device-tree"
-        try:
-            for directoryfolder in os.listdir(prefix):
-                if directoryfolder.find("soc") > -1:
-                    if os.path.exists(prefix + '/' + directoryfolder + '/' + address):
-                        return (prefix + '/' + directoryfolder + '/' + address)
-        except OSError:
-            pass
-        except Exception as exc:
-            pass
-        return None
-
+def found_perf():
+    prefix = "/proc/device-tree"
+    for root, dirs, files in os.walk(prefix):
+        for d in dirs:
+            if 'perf@' in d:
+                return (os.path.join(root, d))
+    return None
 
 with open('/proc/device-tree/compatible', 'r') as compatible:
     compatible_string = compatible.read()
@@ -35,17 +29,27 @@ with open('/proc/device-tree/compatible', 'r') as compatible:
         ddr_type = 'DDR'
         divider = 1
         clock_name = 'pll2_r'
-    elif 'stm32mp25' in compatible_string:
-        word_length  = 4    # Bytes
+    elif 'stm32mp2' in compatible_string:
+        if 'stm32mp25' in compatible_string:
+            # 32 bits
+            word_length  = 4    # Bytes
+            ddr_freq = 1200
+        if 'stm32mp23' in compatible_string:
+            # 32 bits
+            word_length  = 4    # Bytes
+            ddr_freq = 1200
+        if 'stm32mp21' in compatible_string:
+            # 16 bits
+            word_length  = 2    # Bytes
+            ddr_freq = 800
         burst_length = 8    # Words
-        ddr_freq = 1200
         ddr_type = 'DDR'
         divider = 8
-        clock_name = 'ck_icn_ddr'
+        clock_name = 'ck_pll2'
         try:
-            devicetree_path = found_perf_with_address("perf@48041000")
+            devicetree_path = found_perf()
             if devicetree_path is None:
-                print("ERROR: Cannot find perf entry on /proc/device-tree/soc* (%s)" % "perf@48041000")
+                print("ERROR: Cannot find perf entry on /proc/device-tree/soc* (%s)" % "perf@xxxxxxxx")
                 sys.exit(1)
             with open("%s/st,dram-type" % devicetree_path, 'rb') as tmp:
                 t = int.from_bytes(list(tmp.read(4)), byteorder='big', signed=False)
@@ -66,12 +70,25 @@ with open('/proc/device-tree/compatible', 'r') as compatible:
 
 def usage():
     print("Usage:")
-    print("  python stm32_ddr_pmu.py [-d <ddr_freq>] -f <perf_file>")
+    print("  python stm32_ddr_pmu.py -f <perf_file> [-d <ddr_freq>] [-w <word_length>] [-l <burst_length>]")
+    print("    -h: print this help")
     print("    -d ddr_freq: DDR frequency in MHz (%s MHz by default)" % ddr_freq)
-    print("    -f perf_file: text file containing the output of")
     print("    -w word_length: width in bytes of DDR bus (%s by default)" % word_length)
     print("    -l burst_length: length in cycles of DDR burst (%s by default)" % burst_length)
-    print("        perf stat -e stm32_ddr_pmu/read_cnt/,stm32_ddr_pmu/time_cnt/,stm32_ddr_pmu/write_cnt/ -a -o <perf_file> <command>")
+    print("    -f perf_file: text file containing the output of a perf stat command")
+    print("")
+    print("        MP1: perf stat -e stm32_ddr_pmu/perf_op_is_rd/,\\")
+    print("                          stm32_ddr_pmu/perf_op_is_wr/,\\")
+    print("                          stm32_ddr_pmu/time_cn/t -C 0 -o <perf_file> <command>")
+    print("")
+    print("        MP2: perf stat -e stm32_ddr_pmu/dfi_is_rd/,\\")
+    print("                          stm32_ddr_pmu/dfi_is_wr/,\\")
+    print("                          stm32_ddr_pmu/dfi_is_rda/,\\")
+    print("                          stm32_ddr_pmu/dfi_is_wra/,\\")
+    print("                          stm32_ddr_pmu/dfi_is_mwra/,\\")
+    print("                          stm32_ddr_pmu/dfi_is_mwr/,\\")
+    print("                          stm32_ddr_pmu/time_cnt/ -C 0 -o <perf_file> <command>")
+    print("")
     print("The script considers bursts of %s words with %s bytes per word." % (burst_length, word_length))
     sys.exit(2)
 
@@ -81,6 +98,8 @@ dic = {}
 with open('/sys/kernel/debug/clk/' + clock_name + '/clk_rate', 'r') as clk_rate:
     clk_rate_Hz = int(clk_rate.readline().strip())
     clk_rate_MHz = clk_rate_Hz / 1000000
+    if 'stm32mp2' in compatible_string:
+        clk_rate_MHz = clk_rate_MHz * 2
 if (clk_rate_MHz):
     print("Found ddr frequency of %s MHz" % clk_rate_MHz)
     ddr_freq = clk_rate_MHz
@@ -88,7 +107,7 @@ else :
     print('Warning: cannot find ddr clock summary entry, fallback to default value else specified')
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "d:f:")
+    opts, args = getopt.getopt(sys.argv[1:], "hd:f:w:l:")
 except getopt.GetoptError:
     print("Error: invalid option !")
     usage()
@@ -99,11 +118,14 @@ for opt,arg in opts:
     elif opt == '-f':
         perf_file = arg
     elif opt == '-w':
-        word_length = arg
+        word_length = int(arg)
     elif opt == '-l':
-        burst_length = arg
-    else:
+        burst_length = int(arg)
+    elif opt == '-h':
         usage()
+
+if len(opts) == 0:
+    usage()
 
 if perf_file == None:
     print("Error: no perf file !")
@@ -112,15 +134,28 @@ if perf_file == None:
 with open(perf_file) as file:
     lines = file.readlines()
     for line in lines:
-        a = re.match(".* ([0-9]+).*stm32_ddr_pmu\/(.*)\/.*", line)
+        a = re.match(" *([0-9]+) *stm32_ddr_pmu/(.*)/", line)
         try:
             dic[a.groups()[1]] = a.groups()[0]
         except:
             continue
 
 constant = word_length * burst_length * ddr_freq * 1000000 / int(dic['time_cnt']) / (1024 * 1024) / divider
-read_bw = int(dic['read_cnt']) * constant
-write_bw = int(dic['write_cnt']) * constant
+if 'stm32mp1' in compatible_string:
+    read_event = int(dic['perf_op_is_rd'])
+    write_event = int(dic['perf_op_is_wr'])
+elif 'stm32mp2' in compatible_string:
+    read_event = int(dic['dfi_is_rd']) + int(dic['dfi_is_rda'])
+    write_event = int(dic['dfi_is_wr']) + int(dic['dfi_is_wra'])
+
+if 'LPDDR' in ddr_type:
+    try:
+        write_event = write_event + int(dic['dfi_is_mwr']) + int(dic['dfi_is_mwra'])
+    except:
+        pass
+
+read_bw = read_event * constant
+write_bw = write_event * constant
 
 print("R = %s MB/s, W = %s MB/s, R&W = %s MB/s (%s @ %s MHz)" % (read_bw.__round__(),
       write_bw.__round__(), (read_bw + write_bw).__round__(), ddr_type, ddr_freq))
